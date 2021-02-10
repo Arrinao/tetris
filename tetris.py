f222a6e77741da8819a857e3bff7692096ec35ee
Arrinao <arrinao@email.cz>
2021-02-10 20:39:40 +0100

    Added shape_active condition

--- tetris.py
+++ tetris.py
@@ -7,9 +7,7 @@ Created on Mon Jan 25 03:36:28 2021
 import sys
 import pathlib
 import random
-import tkinter
-
-root = tkinter.Tk()
+import time
 
 height=20
 width=12
@@ -17,19 +15,10 @@ width=12
 x = int(width/2)
 y = 0
 
-
-def shape_mover(shape_coords):
-    root.after(500)
-    for shape in shape_coords:
-        print (f'Delete {shape[0]} and {shape[1]}.')
-    shape_coords = [(x, y+1) for x, y in shape_coords]
-    for shape in shape_coords:
-        print(shape[0], shape[1])
-    shape_mover(shape_coords)
-
        
 def shapes():
     shape_choice = random.choice(['L', 'O', 'L_rev', 'E', 'Z', 'Z_rev', 'I'])
+    shape_active = True
     if shape_choice == 'L':
         shape_coords = [(x-1, y), (x,y), (x+1, y), (x+1, y+1)]
         for block_x, block_y in shape_coords:
@@ -65,9 +54,18 @@ def shapes():
         for block_x, block_y in shape_coords:
              print(block_x, block_y)
         shape_mover(shape_coords)
-    
-shapes()
 
+def shape_mover(shape_coords):
+    if shape_active == True:
+        time.sleep(0.5)
+        for shape in shape_coords:
+            print (f'Delete {shape[0]} and {shape[1]}.')
+        shape_coords = [(x, y+1) for x, y in shape_coords]
+        for shape in shape_coords:
+            print(shape[0], shape[1])
+        shape_mover(shape_coords)
+
+shapes()
 
        
 root.mainloop()
