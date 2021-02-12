84ff904592cd28d4ba1cab02c0b40c4cf86ed527
Arrinao <arrinao@email.cz>
2021-02-12 21:39:44 +0100

    Added class for logic, blacked the code

--- tetris.py
+++ tetris.py
@@ -18,8 +18,9 @@ GREEN = (0, 255, 0)
 root = tkinter.Tk()
 root.resizable(False, False)
 
-tetris_canvas = tkinter.Canvas(root, width=400, height=800, background='black')
+tetris_canvas = tkinter.Canvas(root, width=400, height=800, background="black")
 tetris_canvas.grid()
+
 
 class TetrisGUI:
     def __init__(self, speed, canvas):
@@ -36,75 +37,79 @@ class TetrisGUI:
         for x in range(10):
             for y in range(20):
                 y_gap += 25
-                tetris_canvas.create_rectangle(20, 20, 200, 50, fill='red', outline='blue')
+                tetris_canvas.create_rectangle(
+                    20, 20, 200, 50, fill="red", outline="blue"
+                )
             x_gap += 25
 
     def draw_shape():
-
         """
         Draws the different shapes on the board
         """
 
 
-
 tetris_gui = TetrisGUI(game_speed, root)
 
-x = int(width/2)
+x = int(width / 2)
 y = 0
 
-landed_shapes = [(6, 10)]
-shape_choice = []
 
-def shapes():
-    shapes = {
-        'L': [(x-1, y), (x,y), (x+1, y), (x+1, y+1)],
-        'L_rev': [(x-1, y+1), (x-1, y), (x, y), (x+1, y)],
-        'O': [(x-1, y), (x, y), (x-1, y+1), (x, y+1)],
-        'E': [(x, y), (x-1, y+1), (x, y+1), (x+1, y+1)],
-        'Z': [(x-1, y), (x, y), (x, y+1), (x-1, y+1)],
-        'Z_rev': [(x+1, y), (x, y), (x, y+1), (x-1, y+1)],
-        'I': [(x-2, y), (x-1, y), (x, y), (x+1, y)]
-    }
-    shape_choice.append(random.choice(list(shapes.values())))
-    if len(shape_choice) < 2:
-        shape_choice.append(random.choice(list(shapes.values())))
-    print(shape_choice)
-    shape_mover(shape_choice[0])
+class TetrisGame:
+    def __init__(self, landed_points):
+        self.landed_points = [(6, 10)]
+
+    def blocks(self):
+        blocks = {
+            "L": [(x - 1, y), (x, y), (x + 1, y), (x + 1, y + 1)],
+            "L_rev": [(x - 1, y + 1), (x - 1, y), (x, y), (x + 1, y)],
+            "O": [(x - 1, y), (x, y), (x - 1, y + 1), (x, y + 1)],
+            "E": [(x, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)],
+            "Z": [(x - 1, y), (x, y), (x, y + 1), (x - 1, y + 1)],
+            "Z_rev": [(x + 1, y), (x, y), (x, y + 1), (x - 1, y + 1)],
+            "I": [(x - 2, y), (x - 1, y), (x, y), (x + 1, y)],
+        }
+        upcoming_block = random.choice(list(blocks.values()))
+        if len(block_coords) > 1:
+            block_coords = random.choice(list(blocks.values()))
+        else:
+            block_coords = upcoming_block
+
+        print(block_coords)
+        block_mover(block_coords)
+
+    def block_mover(self, block_coords):
+        time.sleep(0.5)
+        for block in block_coords:
+            print(f"Deleted at {block[0]}, {block[1]}.")
+        if any((x, y + 1) in landed_points for (x, y) in block_coords) or any(
+            y + 1 == height for (x, y) in block_coords
+        ):
+            for coord in block_coords:
+                landed_points.append(coord)
+            block_coords.pop(0)
+            print(self.landed_points)
+            blocks()
+        else:
+            block_coords = [(x, y + 1) for x, y in block_coords]
+            for block in block_coords:
+                print(block[0], block[1])
+                block_mover(block_coords)
+
+    def user_input_left(self, event):
+        print("Going left!")
+        for (x, y) in block_coords:
+            return (x - 1, y)
+
+    def user_input_right(self, event):
+        print("Going right!")
+        for (x, y) in block_coords:
+            return (x + 1, y)
 
 
-def shape_mover(shape_coords):
-    time.sleep(0.5)
-    for shape in shape_coords:
-        print (f'Deleted at {shape[0]}, {shape[1]}.')
-    if any((x, y+1) in landed_shapes for (x, y) in shape_coords) or any(y+1 == height for (x, y) in shape_coords):
-        for coord in shape_coords:
-            landed_shapes.append(coord)
-        shape_choice.pop(0)
-        print(landed_shapes)
-        shapes()
-    else:
-        shape_coords = [(x, y+1) for x, y in shape_coords]
-        for shape in shape_coords:
-            print(shape[0], shape[1])
-        shape_mover(shape_coords)
-
-def user_input_left(event):
-    print('Going left!')
-    for (x, y) in shape_coords:
-        return (x-1, y)
+root.bind("<Left>", user_input_left)
+root.bind("<Right>", user_input_right)
 
 
-def user_input_right(event):
-    print('Going right!')
-    for (x, y) in shape_coords:
-        return (x+1, y)
-
-
-root.bind('<Left>', user_input_left)
-root.bind('<Right>', user_input_right)
-
-
-
-#tetris_gui.draw_board()
+# tetris_gui.draw_board()
 root.mainloop()
-shapes()
+blocks()
