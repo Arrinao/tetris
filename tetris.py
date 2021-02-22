6a26e2f55916461802d647c38ea30570e124e619
Akuli <akuviljanen17@gmail.com>
2021-02-22 09:40:09 +0200

    fix attribute name in __init__ (#22)

--- tetris.py
+++ tetris.py
@@ -111,7 +111,7 @@ class TetrisGame:
     def __init__(self):
         self.landed_blocks = []
         self.upcoming_block_shape = None
-        self.upcoming_block_center = None
+        self.current_block_shape = None
         self.current_block_center = None
 
     def new_block(self):
